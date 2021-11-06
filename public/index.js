77b74c6e0a4a8e06e058e32baa4955293ff3d1a0
lucas-bertoni <lbertoni1227@hotmail.com>
2021-11-06 12:33:42 -0400

    Single player works now

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const MAX_GEN_NUM = 1000;
+const MAX_GEN_NUM = 100;
 const NUM_BOXES = 30;
 const maxBoxCount = 20;
 const gameCanvas = document.getElementById("game-canvas");
@@ -60,6 +60,7 @@ function colorBox(x, y, color) {
   let yBoxCoord = Math.floor(y / boxHeight);
   let correctedX = xBoxCoord * boxWidth;
   let correctedY = yBoxCoord * boxHeight;
+
   gameCtx.beginPath();
   gameCtx.fillStyle = color;
   gameCtx.fillRect(correctedX, correctedY, boxWidth, boxHeight);
@@ -100,26 +101,117 @@ function switchTool(tool) {
   }
 }
 
-function startGame() {
+async function startGame() {
   // Scale the game board down so that each box is represented by a single pixel on a new "canvas" represented by a 2D array
-  let gameBoardArray = [];
+  let gameBoardArrayInitial = [];
   for (let row = 0; row < NUM_BOXES; ++row) {
     let innerArr = [];
     for (let boxInRow = 0; boxInRow < NUM_BOXES; ++boxInRow) {
-      let centerCoord = {cx: boxInRow * boxHeight + boxHeight / 2, cy: row * boxHeight + boxHeight / 2}
-      let pcolor = gameCtx.getImageData(centerCoord.cx, centerCoord.cy, 1, 1).data;
+      let cx = row * boxHeight + boxHeight / 2;
+      let cy = boxInRow * boxHeight + boxHeight / 2;
+
+      let pcolor = gameCtx.getImageData(cx, cy, 1, 1).data;
       if (pcolor.filter((e) => e === 0).length === 4 || pcolor.filter((e) => e === 255).length === 4) {
-        innerArr.push({isDead: true, controllingPlayer: null});
+        innerArr.push({isAlive: false, controllingPlayer: null});
       } else {
-        innerArr.push({isDead: false, controllingPlayer: 1});
+        innerArr.push({isAlive: true, controllingPlayer: 1});
       }
     }
-    gameBoardArray.push(innerArr);
+    gameBoardArrayInitial.push(innerArr);
   }
 
-  console.log(gameBoardArray);
+  let previousGen = makeCopy2D(gameBoardArrayInitial);
+  let nextGen = makeCopy2D(gameBoardArrayInitial);
+  function makeCopy2D(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; ++i) {
+      let row = [];
+      for (let j = 0; j < arr[i].length; ++j) {
+        let cellCopy = {isAlive: arr[i][j].isAlive, controllingPlayer: arr[i][j].controllingPlayer};
+        row.push(cellCopy);
+      }
+      newArr.push(row);
+    }
+    return newArr;
+  }
 
   for (let genNum = 0; genNum < MAX_GEN_NUM; ++genNum) {
+    await sleep(250);
+    // console.log("--------------------");
+    // console.log("GENERATION " + (genNum + 1));
+    // console.log("--------------------");
     // Run the simulation
+    for (let col = 0; col < NUM_BOXES; ++col) {
+      for (let row = 0; row < NUM_BOXES; ++row) {
+        // console.log("curCell = (" + row + ", " + col + ")");
+        const cell = previousGen[col][row]; // specific box
+        let numNeighbors = 0; // amount of neighbors cell has
+        for (let i = -1; i <= 1; ++i) { //finding current cell's neighbors. 3x3 square w/o the center square
+          for (let j = -1; j <= 1; ++j) {
+            if (i === 0 && j === 0) { continue; }
+            let x_cell = col + i;
+            let y_cell = row + j;
+
+            if (x_cell >= 0 && y_cell >= 0 && x_cell < NUM_BOXES && y_cell < NUM_BOXES) {
+              let currentNeighbor = previousGen[col + i][row + j];
+              // console.log("cell at (" + row + ", " + col + ")" + (currentNeighbor.isDead ? " is dead" : " is alive"));
+              if (currentNeighbor.isAlive) { numNeighbors += 1; }
+            }
+          }
+        }
+
+        // console.log("cell at (" + row + ", " + col + ") has " + numNeighbors + " neighbors");
+
+        //rules of game
+        //underpopulation
+        if(cell.isAlive && numNeighbors < 2) { //if cur cell has less than 2 neighbors
+          // console.log("Killing cell at (" + row + ", " + col + ") underpopulation");
+          nextGen[col][row].isAlive = false;
+          nextGen[col][row].controllingPlayer = null;
+        }
+        //overpopulation
+        else if(cell.isAlive && numNeighbors > 3 ) { //if cur cell has more than 3 neighbors
+          // console.log("Killing cell at (" + row + ", " + col + ") overpopulation");
+          nextGen[col][row].isAlive = false;
+          nextGen[col][row].controllingPlayer = null;
+        }
+        //revive
+        else if(!cell.isAlive && numNeighbors === 3 ) { //if cur cell has exactly 3 neighbors and is dead
+          // console.log("Reviving cell at (" + row + ", " + col + ")");
+          nextGen[col][row].isAlive = true;
+          nextGen[col][row].controllingPlayer = 1;
+        }
+      }
+    }
+    await(showOnScreen(nextGen));
+    previousGen = makeCopy2D(nextGen);
   }
 }
+
+function showOnScreen(generationInfo) {
+  // console.log(generationInfo);
+  for (let i = 0; i < generationInfo.length; ++i) {
+    for (let j = 0; j < generationInfo[i].length; ++j) {
+      // scale the coordinates back up to full size
+      let x = i * boxWidth + boxWidth / 2;
+      let y = j * boxHeight + boxHeight / 2;
+      // console.log("x: " + x + "   y: " + y);
+      if (generationInfo[i][j].isAlive) {
+        colorBox(x, y, "#FF0000");
+      } else {
+        colorBox(x, y, "#FFFFFF");
+      }
+      // gameCtx.beginPath();
+      // if (i >= 3 && i <= 10) {
+      //   gameCtx.fillStyle = "blue";
+      // }
+      // if (j >= 3 && j <= 10) {
+      //   gameCtx.fillStyle = "blue";
+      // }
+      // gameCtx.fillRect(x - 4, y - 4, 8, 8);
+      // gameCtx.stroke();
+    }
+  }
+}
+
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
